35e3d00b0287b45ae07648471e5d521cee5fc07e
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-07 15:41:05 +0530

    feat:updated darkmode.js fixed typo in aria-pressed

--- darkmode.js
+++ darkmode.js
@@ -1,24 +1,24 @@
 const toggleElement=document.getElementById("checkbox");
 toggleElement.addEventListener("click",function (event){
-    if(this.ariaPressed==="true"){
-        this.ariaPressed=false;
+    if(this.getAttribute("aria-pressed")==="true"){
+        this.setAttribute("aria-pressed","false");
         document.documentElement.setAttribute("data-theme","light");
         localStorage.setItem("data-theme","light");
     }else{
-        this.ariaPressed="true";
+        this.setAttribute("aria-pressed","true");
         document.documentElement.setAttribute("data-theme","dark");
         console.log("switched to dark");
         localStorage.setItem("data-theme","dark");
     }
 });
-window.onload=(event)=>{
+window.addEventListener('DOMContentLoaded',()=>{
     let theme=localStorage.getItem("data-theme");
     if(theme==="dark"){
         document.documentElement.setAttribute("data-theme","dark");
-        toggleElement.setAttribute("ariaPressed","true");
+        toggleElement.setAttribute("aria-pressed","true");
 
     }else{
         document.documentElement.setAttribute("data-theme","light");
-        toggleElement.setAttribute("ariaPressed","false");
+        toggleElement.setAttribute("aria-pressed","false");
     }
-}
+})
