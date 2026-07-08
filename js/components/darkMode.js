c2b625e2dd9ded4c1df259ae22a440572a8d3d9b
aswinvijayan-vonnue <aswin.vijayan@vonnue.com>
2026-07-08 16:25:17 +0530

    refactor: rearranged darkMode.js

--- js/components/darkMode.js
+++ js/components/darkMode.js
@@ -1,11 +1,24 @@
-export function darkMode(event) {
-  const isPressed = this.getAttribute("aria-pressed");
-  if (isPressed) {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("data-theme", "light");
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("data-theme", "dark");
-  }
-  this.setAttribute("aria-pressed", !isPressed);
-}
+const toggleElement=document.getElementById("checkbox");
+toggleElement.addEventListener("click",function (event){
+    if(this.getAttribute("aria-pressed")==="true"){
+        this.setAttribute("aria-pressed","false");
+        document.documentElement.setAttribute("data-theme","light");
+        localStorage.setItem("data-theme","light");
+    }else{
+        this.setAttribute("aria-pressed","true");
+        document.documentElement.setAttribute("data-theme","dark");
+        console.log("switched to dark");
+        localStorage.setItem("data-theme","dark");
+    }
+});
+window.addEventListener('DOMContentLoaded',()=>{
+    let theme=localStorage.getItem("data-theme");
+    if(theme==="dark"){
+        document.documentElement.setAttribute("data-theme","dark");
+        toggleElement.setAttribute("aria-pressed","true");
+
+    }else{
+        document.documentElement.setAttribute("data-theme","light");
+        toggleElement.setAttribute("aria-pressed","false");
+    }
+})
